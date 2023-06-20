dc3277bd36fdbf8f9e4a54c6595aaa14fc4ffbce
Sarah Jacobs <sgjacobs14@gmail.com>
2023-06-20 13:29:10 -0400

    working on cors

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-app.get('/test', (req, res) => {
+app.use(cors());
+
+app.post('/register', (req, res) => {
     res.json('it worked')
 })
 
