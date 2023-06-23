df7d3e0d944953b9c3178940f121b6ceb69a6cb5
Sarah Jacobs <sgjacobs14@gmail.com>
2023-06-23 16:29:12 -0400

    completed registration function

--- api/index.js
+++ api/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+const User = require('./models/User');
+
+var salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
+app.use(express.json());
 
-app.post('/register', (req, res) => {
-    res.json('it worked')
+mongoose.connect('mongodb+srv://blog:StvdR1AGVtN0kCPD@blog.v6adnmd.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.create({
+        username, 
+        password: bcrypt.hashSync(password, salt),
+    })
+    res.json(userDoc)
 })
 
 console.log('test')
